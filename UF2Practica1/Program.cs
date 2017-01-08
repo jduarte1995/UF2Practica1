435a4e9a5b7f0cf571c246af7dc7021ce8f982af
jduarte1995 <jesus.duarte.rebollar@gmail.com>
2017-01-09 00:29:37 +0100

    uf2 practica1 acabada

--- UF2Practica1/Program.cs
+++ UF2Practica1/Program.cs
@@ -11,8 +11,9 @@ namespace UF2Practica1
 	{
 		//Valors constants
 		#region Constants
-		const int nCaixeres = 3;
-
+	
+        const int tCaixes = 10;//per si es canvia al text, mes rapid
+       
 		#endregion
 		/* Cua concurrent
 		 	Dos mètodes bàsics: 
@@ -24,8 +25,23 @@ namespace UF2Practica1
 
 		public static void Main(string[] args)
 		{
-			var clock = new Stopwatch();
+            int i;
+            int caixeres;
+           
+            Console.WriteLine("**********************");
+            Console.WriteLine("*     APU MARKET     *");
+            Console.WriteLine("**********************");
+
+            //primera accio, demanar caixeres
+            Console.WriteLine("Introdueix el numero de caixeres.");
+            Console.WriteLine("Hi ha un total de " + tCaixes + " caixes.");
+            Caixera c = new UF2Practica1.Caixera() ;//instancio un objecte de la clase caixera per accedir al metode que demana les caixeres.
+            caixeres = c.triaCaixeres();
+           
+
+            var clock = new Stopwatch();
 			var threads = new List<Thread>();
+
 			//Recordeu-vos que el fitxer CSV ha d'estar a la carpeta bin/debug de la solució
 			const string fitxer = "CuaClients.csv";
 
@@ -41,7 +57,7 @@ namespace UF2Practica1
 					var line = reader.ReadLine();
 					var values = line.Split(';');
 					var tmp = new Client() { nom = values[0], carretCompra = Int32.Parse(values[1]) };
-					cua.Enqueue(tmp);
+					cua.Enqueue(tmp);//Enqueue equeueeja clients a la cua
 
 				}
 
@@ -56,15 +72,19 @@ namespace UF2Practica1
 			clock.Start();
 
 
-			// Instanciar les caixeres i afegir el thread creat a la llista de threads
-
-
-
+            // Instanciar les caixeres i afegir el thread creat a la llista de threads
+            for (i = 0; i < caixeres; i++)
+            {
+                var cTemp = new UF2Practica1.Caixera() { idCaixera = i };
+                var Thread = new Thread(() => cTemp.ProcessarCua());
+                Thread.Start();
+                threads.Add(Thread);
+            }
 
 			// Procediment per esperar que acabin tots els threads abans d'acabar
 			foreach (Thread thread in threads)
-				thread.Join();
-
+				thread.Join();//bloqueja fil fins que acaba la cua???/******* aclarir amb en Carlos *******/
+            
 			// Parem el rellotge i mostrem el temps que triga
 			clock.Stop();
 			double temps = clock.ElapsedMilliseconds / 1000;
@@ -73,10 +93,12 @@ namespace UF2Practica1
 			Console.ReadKey();
 		}
 	}
+   
 	#region ClassCaixera
 	public class Caixera
 	{
-		public int idCaixera
+
+        public int idCaixera
 		{
 			get;
 			set;
@@ -84,14 +106,18 @@ namespace UF2Practica1
 
 		public void ProcessarCua()
 		{
-			// Llegirem la cua extreient l'element
-			// cridem al mètode ProcesarCompra passant-li el client
+            // Llegirem la cua extreient l'element
+            // cridem al mètode ProcesarCompra passant-li el client
 
+            while (!MainClass.cua.IsEmpty) {//mentre no estigu buida  
+                var temp = new Client();
+                MainClass.cua.TryDequeue(out temp);// TryDequeue desqueueja de la cua
+                ProcesarCompra(temp);
+            }
 
 
 		}
-
-
+        
 		private void ProcesarCompra(Client client)
 		{
 
@@ -109,9 +135,37 @@ namespace UF2Practica1
 
 		private void ProcessaProducte()
 		{
-			Thread.Sleep(TimeSpan.FromSeconds(1));
+			Thread.Sleep(TimeSpan.FromSeconds(1));//retrasa 1 segon
 		}
-	}
+
+        public int triaCaixeres()//demana caixeres
+        {
+          
+            int tCaixeres = 0;
+           
+
+            String totalCaixeres = Console.ReadLine();
+            bool isInt = Int32.TryParse(totalCaixeres, out tCaixeres);//conversió a integer SEMPRE OUT try parse
+            if (!isInt) 
+                Console.WriteLine("Entra un valor correcte");
+        
+                comprovaResposta(tCaixeres);
+            return tCaixeres;
+
+        }
+
+
+        public void comprovaResposta( int tCaixeres) {
+            if (tCaixeres < 1 || tCaixeres > 10)
+            {
+                Console.WriteLine("Entra un valor correcte entre 1 i 10");
+                triaCaixeres();
+            }
+            else
+                Console.WriteLine("Has donat d'alta " + tCaixeres + " caixeres");
+            
+        }
+    }
 
 
 	#endregion
